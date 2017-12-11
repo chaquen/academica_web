02b60b2cfd6c2ff24fb7b135bfdfe67e87325884
chaquen <edgar.guzman21@gmail.com>
2017-12-10 19:59:15 -0500

    se cambia ruta de gitignore

--- js/globales.js
+++ js/globales.js
@@ -4,8 +4,8 @@
 //variable que almacena el usuario
 
 var globales={
-	//_URL:"http://localhost/api_academica/",
-	_URL:"https://apiacademica.mohansoft.com/",
+	_URL:"http://localhost/api_academica/",
+	//_URL:"https://apiacademica.mohansoft.com/",
 	//_URL:"https://",
 	_usuario:false,
 	_cerrar_sesion:false,
