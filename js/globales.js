8ced37fea76f5a58765e3dc7c987bde442e91aa3
chaquen <edgar.guzman21@gmail.com>
2017-12-10 21:18:25 -0500

    se actualiza globales

--- js/globales.js
+++ js/globales.js
@@ -4,7 +4,7 @@
 //variable que almacena el usuario
 
 var globales={
-	//_URL:"http://localhost/api_academica/",
+	_URL:"http://localhost/api_academica/",
 	//_URL:"https://apiacademica.mohansoft.com/",
 	//_URL:"https://",
 	_usuario:false,
